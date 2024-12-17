38e41c3898b45489baffd6a141b8b88cc751cb61
julienginest <julien.ginestiere+git@gmail.com>
2024-12-17 16:34:33 +0100

    Modification de la BDD

--- BDD.sql
+++ BDD.sql
@@ -1,31 +1,35 @@
-DROP DATABASE IF EXISTS Istres;
+CREATE DATABASE `Istres`;
+USE `Istres`;
 
-CREATE DATABASE Istres;
-
-USE Istres;
-
-CREATE TABLE `Planes` (
-	`registration` VARCHAR(10) NOT NULL,
-	`brand` VARCHAR(100) NOT NULL,
-	`model` VARCHAR(100),
-	PRIMARY KEY (`registration`)
-);
-
-CREATE TABLE `Technicians` (
-	`Id` INT NOT NULL AUTO_INCREMENT,
-	`firstName` VARCHAR(50) NOT NULL,
-	`surName` VARCHAR(50) NOT NULL,
-	PRIMARY KEY (`Id`)
-);
-
+DROP TABLE IF EXISTS `Maintenances`;
 CREATE TABLE `Maintenances` (
-	`Id` INT NOT NULL AUTO_INCREMENT,
-	`title` VARCHAR(80) NOT NULL,
-	`technicianID` INT NOT NULL,
-	`planeRegistration` VARCHAR(10) NOT NULL,
-	`start` DATETIME NOT NULL,
-	`end` DATETIME,
-	PRIMARY KEY (`Id`),
-    CONSTRAINT `fk_Flights-Technicians` FOREIGN KEY (technicianID) REFERENCES Technicians (Id),
-    CONSTRAINT `fk_Flights-Planes` FOREIGN KEY (planeRegistration) REFERENCES Planes (registration)
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(80) NOT NULL,
+  `technicianID` int(11) NOT NULL,
+  `planeRegistration` varchar(10) NOT NULL,
+  `start` datetime NOT NULL,
+  `end` datetime DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `fk_Flights-Technicians` (`technicianID`),
+  KEY `fk_Flights-Planes` (`planeRegistration`),
+  CONSTRAINT `fk_Flights-Planes` FOREIGN KEY (`planeRegistration`) REFERENCES `Planes` (`registration`),
+  CONSTRAINT `fk_Flights-Technicians` FOREIGN KEY (`technicianID`) REFERENCES `Technicians` (`Id`)
+);
+
+
+DROP TABLE IF EXISTS `Planes`;
+CREATE TABLE `Planes` (
+  `registration` varchar(10) NOT NULL,
+  `brand` varchar(100) NOT NULL,
+  `model` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`registration`)
+);
+
+
+DROP TABLE IF EXISTS `Technicians`;
+CREATE TABLE `Technicians` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(50) NOT NULL,
+  `surName` varchar(50) NOT NULL,
+  PRIMARY KEY (`Id`)
 );
